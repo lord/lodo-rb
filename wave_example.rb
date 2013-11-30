a22b4b7b406d4945b3fcc0f323415793f11617a3
Robert Lord <robert@lord.io>
2013-11-29 22:56:18 -0800

    Add wave example
    This wave example displays "waves" of alternating color that move
    down the LED strand.

---
+++ wave_example.rb
@@ -0,0 +1,47 @@
+require './lodo'
+
+lights = LightString.new
+lights.reset
+
+brightness = 0
+going_up = true
+colors = [
+  {r: 1, g: 0, b: 0},
+  {r: 0, g: 1, b: 0},
+  {r: 0, g: 0, b: 1},
+  {r: 0, g: 1, b: 1},
+  {r: 1, g: 0, b: 1},
+  {r: 0.5, g: 0, b: 1},
+  {r: 1, g: 1, b: 0},
+  {r: 1, g: 1, b: 1}
+]
+color = colors.sample
+
+loop do
+  (LightString::LED_COUNT - 1).downto(0) do |light_number|
+    if light_number == 0
+      lights[light_number] = {red: brightness.abs * color[:r], green: brightness.abs * color[:g], blue: brightness.abs * color[:b]}
+    else
+      lights[light_number] = lights[light_number - 1]
+    end
+  end
+
+  lights.push
+
+  if going_up
+    brightness += 10
+    if brightness > 255
+      going_up = false
+      brightness = 255
+    end
+  else
+    brightness -= 10
+    if brightness < 0
+      going_up = true
+      brightness = 0
+      color = colors.sample
+    end
+  end
+
+  sleep 0.03
+end
