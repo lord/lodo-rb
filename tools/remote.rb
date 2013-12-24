10329b631b8ff37742f23e610936ee3f7bb70d7f
Robert Lord <robert@lord.io>
2013-12-23 18:20:56 -0800

    Update remote, switch to sinatra modular

--- tools/remote.rb
+++ tools/remote.rb
@@ -1,19 +1,24 @@
-require 'sinatra'
+require 'sinatra/base'
 require './lib/lodo'
 
-board = Lodo::Board.new
+class Remote < Sinatra::Base
 
-set :views, Proc.new { File.join(root, "..", "templates") }
+  board = Lodo::Board.new
 
-set :bind, '0.0.0.0'
+  set :views, Proc.new { File.join(root, "..", "templates") }
 
-get '/' do
-  board.reset
-  erb :remote
-end
+  set :bind, '0.0.0.0'
 
-get '/set' do
-  board.draw_pixel(params[:x].to_i, params[:y].to_i, {red: params[:r].to_i, green: params[:g].to_i, blue: params[:b].to_i})
-  board.save
-  "OK"
+  get '/' do
+    board.reset
+    erb :remote
+  end
+
+  get '/set' do
+    board.draw_pixel(params[:x].to_i, params[:y].to_i, {red: params[:r].to_i, green: params[:g].to_i, blue: params[:b].to_i})
+    board.save
+    "OK"
+  end
+
+  run!
 end
