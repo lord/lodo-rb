fea1db657d4f575285ffd4784e071143f616ce64
Robert Lord <robert@lord.io>
2013-12-23 18:04:03 -0800

    Update String to use internal reference for Core
    This will make implementation of CoreSimulator easier later.

--- lib/lodo/string.rb
+++ lib/lodo/string.rb
@@ -3,22 +3,23 @@ module Lodo
     LED_COUNT = 100
 
     def initialize
-      @device = Core.open_device
-      @buffer = Core::Buffer.new
+      @core = Core
+      @device = @core.open_device
+      @buffer = @core::Buffer.new
       @buffer_cache = {} # TODO replace with retrieval from C buffer?
 
-      Core.set_gamma 2.2, 2.2, 2.2
+      @core.set_gamma 2.2, 2.2, 2.2
       raise "Device failed to open" if @device <= 0
 
-      spi_status = Core.spi_init(@device)
+      spi_status = @core.spi_init(@device)
       raise "SPI failed to start" if spi_status != 0
 
-      tcl_status = Core.tcl_init(@buffer.pointer, LED_COUNT)
+      tcl_status = @core.tcl_init(@buffer.pointer, LED_COUNT)
       raise "TCL failed to start" if tcl_status != 0
     end
 
     def []= (led_number, color)
-      Core.write_gamma_color_to_buffer(@buffer.pointer, led_number, color[:red], color[:green], color[:blue])
+      @core.write_gamma_color_to_buffer(@buffer.pointer, led_number, color[:red], color[:green], color[:blue])
       @buffer_cache[led_number] = color
     end
 
@@ -27,7 +28,7 @@ module Lodo
     end
 
     def save
-      Core.send_buffer(@device, @buffer.pointer)
+      @core.send_buffer(@device, @buffer.pointer)
     end
 
     def each
